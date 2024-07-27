c8d2a04cc0ced5a785665f7e5dd1e8c3c47faac7
Deepanshu Taneja <120954466+Taneja12@users.noreply.github.com>
2024-07-27 13:22:22 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'https://mern-book-store-gilt.vercel.app/'; // Adjust port if necessary
+const BASE_URL = 'https://mern-book-store-gilt.vercel.app'; // Adjust port if necessary
 
 export const fetchBooks = async ({ page = 1, pageSize = 10 } = {}) => {
   try {
