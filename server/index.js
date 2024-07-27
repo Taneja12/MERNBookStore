b3da2262bcdff28c7c8835e2c419e13f4a011b9b
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-07-27 14:07:15 +0530

    h

--- server/index.js
+++ server/index.js
@@ -17,10 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
