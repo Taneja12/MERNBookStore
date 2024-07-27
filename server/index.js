78bb61909dc8e36d074a8e12efedf249e469925a
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-07-27 12:53:01 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -43,11 +49,11 @@ app.use('/api/auth', authRouter);
 // Endpoint for creating orders and payment session ID
 app.use('/api/orders', ordercreationRoutes);
 
-app.use('/api/cart' , cartRoutes);
+app.use('/api/cart', cartRoutes);
 
-app.use('/api/admin' , adminRoutes);
+app.use('/api/admin', adminRoutes);
 
-app.use('/api/contact',contactRouter);
+app.use('/api/contact', contactRouter);
 
 // Start server
 app.listen(PORT, () => {
