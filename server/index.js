1899b721cf642b533d4aecc11094ba92eeeaaea3
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-07-27 14:00:06 +0530

    casd

--- server/index.js
+++ server/index.js
@@ -8,26 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors(
-  {
-    origin: "https://mern-book-store-ajdb-frontend.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: "https://mern-book-store-ajdb-frontend.vercel.app",
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
-// mongoose.connect('mongodb://127.0.0.1:27017/mydatabase');
-mongoose.connect(process.env.MONGODB_URI);
-
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 const bookRouter = require('./routes/bookRouter');
@@ -41,20 +36,14 @@ const contactRouter = require('./routes/contactRouter');
 app.get('/', (req, res) => {
   res.json('API is working!');
 });
-// Endpoint for books and categories
+
+// Define API routes
 app.use('/api/books', bookRouter);
 app.use('/api/category', catRouter);
-
-// Endpoint for authentication
 app.use('/api/auth', authRouter);
-
-// Endpoint for creating orders and payment session ID
 app.use('/api/orders', ordercreationRoutes);
-
 app.use('/api/cart', cartRoutes);
-
 app.use('/api/admin', adminRoutes);
-
 app.use('/api/contact', contactRouter);
 
 // Start server
